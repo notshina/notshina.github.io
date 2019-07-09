c3b2c89081299a82b5b4fbfbbadddc140e0d803f
Shina <your.lame@Yahoo.com>
2019-07-09 14:58:31 -0700

    Initial Commit

---
+++ aboutme.js
@@ -0,0 +1,12 @@
+alert("Welcome to my Page!");
+
+function bigImg(x){
+  x.style.height = "400px";
+  x.style.width = "400px";
+
+}
+
+function smallImg(x){
+  x.style.height = "200px";
+  x.style.width  = "200px";
+}
